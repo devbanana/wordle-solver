3a4f588006b54f79a5674f771f3ccd61525c557e
Brandon Olivares <programmer2188@gmail.com>
2022-02-13 23:20:27 -0500

    Add endpoint to guess a word

--- src/game.controller.ts
+++ src/game.controller.ts
@@ -1,14 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { GameService } from './game.service';
 
 @Controller('game')
 export class GameController {
   constructor(private readonly game: GameService) {}
 
-  @Get('start')
-  start(date: string): { game: string } {
+  @Get('start/:date')
+  start(@Param('date') date: string): { game: string } {
     const game = this.game.start(date);
 
     return { game };
+  }
+
+  @Get(':token/guess/:word')
+  guess(
+    @Param('token') token: string,
+    @Param('word') word: string,
+  ): { words: string[] } {
+    const words = this.game.guess(token, word);
+
+    return { words };
   }
 }
