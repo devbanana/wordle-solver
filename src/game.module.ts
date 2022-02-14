e9c0e15016f19fb767f5f88df081ebdca987d503
Brandon Olivares <programmer2188@gmail.com>
2022-02-13 19:43:28 -0500

    Add WordleEvaluatorService

--- src/game.module.ts
+++ src/game.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GameController } from './game.controller';
 import { GameService } from './game.service';
+import { WordleEvaluatorService } from './wordle-evaluator.service';
 
 @Module({
   imports: [],
   controllers: [GameController],
-  providers: [GameService],
+  providers: [GameService, WordleEvaluatorService],
 })
 export class GameModule {}
