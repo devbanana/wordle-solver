bff3a7b67d0daa07f07779eb49fde3f10146fbde
Brandon Olivares <programmer2188@gmail.com>
2022-02-13 19:44:01 -0500

    Add complete handling of guesses

--- src/guess-narrower.spec.ts
+++ src/guess-narrower.spec.ts
@@ -1,20 +1,135 @@
 import { GuessNarrower } from './guess-narrower';
+import { WordleEvaluatorService } from './wordle-evaluator.service';
+import { Test } from '@nestjs/testing';
 
-const wordList = ['ultra', 'angry', 'silly', 'furry'];
+const wordList = [
+  'alert',
+  'angry',
+  'being',
+  'blurb',
+  'fairy',
+  'flirt',
+  'furry',
+  'glare',
+  'glory',
+  'golem',
+  'guard',
+  'icily',
+  'ledge',
+  'lemon',
+  'lodge',
+  'notch',
+  'plant',
+  'revel',
+  'ruler',
+  'silly',
+  'slope',
+  'sloth',
+  'total',
+  'ultra',
+  'umbra',
+  'wheel',
+  'whirl',
+  'whole',
+  'zebra',
+];
 
 describe('GuessNarrower', () => {
+  let narrower: GuessNarrower;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [WordleEvaluatorService],
+    }).compile();
+
+    narrower = new GuessNarrower(
+      'ultra',
+      wordList,
+      module.get<WordleEvaluatorService>(WordleEvaluatorService),
+    );
+  });
+
   it('should be defined', () => {
-    expect(new GuessNarrower('ultra', [])).toBeDefined();
+    expect(narrower).toBeDefined();
   });
 
   it('should narrow based on correct letter', () => {
-    const narrower = new GuessNarrower('ultra', wordList);
-
     const result = narrower.guess('soare');
 
-    expect(result.length).toBe(3);
-    expect(result[0]).toBe('angry');
-    expect(result[1]).toBe('furry');
-    expect(result[2]).toBe('ultra');
+    expect(result).toStrictEqual(['angry', 'fairy', 'ultra', 'umbra']);
+  });
+
+  it('should narrow based on multiple correct guesses', () => {
+    narrower.guess('soare');
+    const result = narrower.guess('hydra');
+
+    expect(result).toStrictEqual(['ultra', 'umbra']);
+  });
+
+  it("should correctly narrow even if second guess doesn't use first correct letter", () => {
+    narrower.guess('sorry');
+    const result = narrower.guess('mocha');
+
+    expect(result).toStrictEqual(['ultra', 'zebra']);
+  });
+
+  it('should narrow based on present letter', () => {
+    const result = narrower.guess('icily');
+
+    expect(result).toStrictEqual([
+      'alert',
+      'blurb',
+      'glare',
+      'golem',
+      'ledge',
+      'lemon',
+      'lodge',
+      'plant',
+      'revel',
+      'ruler',
+      'slope',
+      'sloth',
+      'total',
+      'ultra',
+      'wheel',
+    ]);
+  });
+
+  it('should narrow based on multiple present letters', () => {
+    narrower.guess('icily');
+    const result = narrower.guess('revel');
+
+    expect(result).toStrictEqual(['blurb', 'ultra']);
+  });
+
+  it('should narrow based on absent letters', () => {
+    const result = narrower.guess('being');
+
+    expect(result).toStrictEqual(['furry', 'sloth', 'total', 'ultra']);
+  });
+
+  it('should only process an absent letter once', () => {
+    const result = narrower.guess('silly');
+
+    expect(result).toStrictEqual([
+      'alert',
+      'blurb',
+      'glare',
+      'ledge',
+      'lemon',
+      'lodge',
+      'plant',
+      'revel',
+      'total',
+      'ultra',
+      'wheel',
+    ]);
+  });
+
+  it('should keep a correct letter even when absent in another guess', () => {
+    narrower.guess('sword');
+    const result = narrower.guess('audio');
+
+    expect(result).toStrictEqual(['ultra', 'umbra']);
   });
 });
--- src/guess-narrower.ts
+++ src/guess-narrower.ts
@@ -1,36 +1,104 @@
 import { States } from './states';
+import { WordleEvaluatorService } from './wordle-evaluator.service';
 
 export class GuessNarrower {
-  private readonly state: States[];
-  private readonly processedCorrect: boolean[];
+  private readonly correctPositions: string[];
+  private readonly presentPositions: Set<string>;
+  private readonly absentPositions: Array<Set<string>>;
 
   constructor(
     private readonly answer: string,
     private readonly wordList: string[],
+    private readonly evaluator: WordleEvaluatorService,
   ) {
-    this.state = Array<States>(answer.length).fill(States.Absent);
-    this.processedCorrect = Array<boolean>(answer.length).fill(false);
+    this.correctPositions = [];
+    this.presentPositions = new Set<string>();
+    this.absentPositions = [
+      new Set<string>(),
+      new Set<string>(),
+      new Set<string>(),
+      new Set<string>(),
+      new Set<string>(),
+    ];
   }
 
   guess(word: string): string[] {
-    for (let i = 0; i < word.length; i++) {
-      if (word[i] === this.answer[i] && !this.processedCorrect[i]) {
-        this.state[i] = States.Correct;
-        this.processedCorrect[i] = true;
-      }
-    }
+    const state = this.evaluator.evaluate(word, this.answer);
 
-    const regex = Array(word.length).fill('.');
-    for (let i = 0; i < word.length; i++) {
-      if (this.state[i] === States.Correct) {
-        regex[i] = word[i];
+    state.forEach((state, index) => {
+      const letter = word[index];
+
+      switch (state) {
+        case States.Correct:
+          this.setCorrectLetter(index, letter);
+          break;
+
+        case States.Present:
+          this.addPossibleLetter(index, letter);
+          break;
+
+        case States.Absent:
+          this.addAbsentLetter(index, letter);
+          break;
       }
-    }
+    });
+
+    const regex = this.calculateRegex(word);
 
     return this.wordList
-      .filter(value => {
-        return RegExp(regex.join('')).exec(value);
-      })
+      .filter(value => RegExp(regex).exec(value))
+      .filter(value =>
+        [...this.presentPositions].every(letter => value.includes(letter)),
+      )
       .sort();
+  }
+
+  private setCorrectLetter(index: number, letter: string): void {
+    this.correctPositions[index] = letter;
+    this.absentPositions[index].clear();
+  }
+
+  private addPossibleLetter(index: number, letter: string): void {
+    if (this.isPositionCorrect(index)) {
+      return;
+    }
+
+    this.absentPositions[index].add(letter);
+    this.presentPositions.add(letter);
+  }
+
+  private addAbsentLetter(index: number, letter: string) {
+    if (this.isPositionCorrect(index)) {
+      return;
+    }
+
+    if (
+      this.correctPositions.includes(letter) ||
+      this.presentPositions.has(letter)
+    ) {
+      this.absentPositions[index].add(letter);
+    } else {
+      for (let i = 0; i < this.answer.length; i++) {
+        if (!this.isPositionCorrect(i)) {
+          this.absentPositions[i].add(letter);
+        }
+      }
+    }
+  }
+
+  private isPositionCorrect(index: number): boolean {
+    return this.correctPositions[index] !== undefined;
+  }
+
+  private calculateRegex(word: string): string {
+    const regex = Array<string>(word.length).fill('.');
+    this.correctPositions.forEach((letter, index) => (regex[index] = letter));
+    this.absentPositions.forEach((letters, index) => {
+      if (letters.size) {
+        regex[index] = `[^${[...letters].sort().join('')}]`;
+      }
+    });
+
+    return regex.join('');
   }
 }
